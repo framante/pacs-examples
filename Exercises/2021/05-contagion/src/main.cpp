4f5a37ff8833718bdfba7e92283c779fe3fe63dc
Francesco Mantegazza <framante95@gmail.com>
2021-05-06 12:12:10 +0000

    nothing to report

--- Exercises/2021/05-contagion/src/main.cpp
+++ Exercises/2021/05-contagion/src/main.cpp
@@ -6,11 +6,12 @@ main(int argc, char **argv)
   Contagion contagion("params.pot");
   contagion.run();
   Domain d("params.pot");
-  std::cout << d.domain_size << " " << d.pub_size << std::endl;
+  //std::cout << "domain size: " <<  d.domain_size << std::endl;
+  //std::cout << "pub_size:  " << d.pub_size << std::endl;
   ContagionParameters cp("params.pot");
-  std::cout << cp.n_init_infected << std::endl;
+  //std::cout << "initial number of infected: " << cp.n_init_infected << std::endl;
   PersonParameters pp("params.pot");
-  std::cout << pp.n_timesteps_recover << std::endl;
+  
   
   return 0;
 }
